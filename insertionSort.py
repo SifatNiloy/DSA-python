90fc19e2f1fa9a228c9ed5bea3d72a63e7601fb5
SifatNiloy <sifatniloy18@gmail.com>
2023-03-07 14:41:27 +0600

    insertion sort changed variable

--- insertionSort.py
+++ insertionSort.py
@@ -1,16 +1,17 @@
+# i=step 
+
 def insertionSort(array):
-    for step in range(1, len(array)):
-        key = array[step]
-        j = step-1
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i-1
         while j >= 0 and key < array[j]:
             array[j+1] = array[j]
             j = j-1
-            print(array, "j = j-1")
+            
         array[j+1] = key
-        print(array, "array[j+1] = key")
+        
 
-
-data = [9, 5, 1, 4, 3]
+data = [9, 5, 1, 4, 3, 15, 21, 13, 8, -4, 0, 11, 15, -2]
 insertionSort(data)
 print('Sorted Array in Ascending Order:')
 print(data)
